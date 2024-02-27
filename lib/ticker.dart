c7516881b38ef0505120fca6c6a4305b3d0a969d
José Carlos Valenzuela Nieto <valenzuelanietojosecarlos@gmail.com>
2024-02-27 17:21:36 +0100

    BlocStatusEmoji

---
+++ lib/ticker.dart
@@ -0,0 +1,11 @@
+// The ticker will be the data source for the
+//timer application.It will expose a stream of ticks.
+
+class Ticker {
+  const Ticker();
+
+  Stream<int> tick({required int ticks}) {
+    return Stream.periodic(const Duration(seconds: 1), (x) => ticks - x - 1)
+        .take(ticks);
+  }
+}
---
+++ lib/timer/view/timer_page.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_timer/ticker.dart';
+import 'package:flutter_timer/timer/bloc/timer_bloc.dart';
+import 'package:flutter_timer/timer/timer.dart';
+
+class TimerPage extends StatelessWidget {
+  const TimerPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => TimerBloc(ticker: Ticker()),
+      child: const TimerView(),
+    );
+  }
+}
+
+class TimerView extends StatelessWidget {
+  const TimerView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Timer')),
+      body: Stack(
+        children: [
+          const Background(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 100.0),
+                child: Center(child: TimerEmoji()),
+              ),
+              Actions(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TimerEmoji extends StatelessWidget {
+  const TimerEmoji({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final duration = context.select((TimerBloc bloc) => bloc.state.duration);
+    final secondsStr = (duration % 60).floor();
+    return Text(
+      getEmojiByIndex(secondsStr),
+      style: Theme.of(context).textTheme.displayLarge,
+    );
+  }
+}
+
+class Actions extends StatelessWidget {
+  const Actions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TimerBloc, TimerState>(
+      buildWhen: (prev, state) => prev.runtimeType != state.runtimeType,
+      builder: (context, state) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ...switch (state) {
+              TimerInitial() => [
+                  FloatingActionButton(
+                    child: const Icon(Icons.play_arrow),
+                    onPressed: () => context
+                        .read<TimerBloc>()
+                        .add(TimerStarted(duration: state.duration)),
+                  ),
+                ],
+              TimerRunInProgress() => [
+                  FloatingActionButton(
+                    child: const Icon(Icons.pause),
+                    onPressed: () =>
+                        context.read<TimerBloc>().add(const TimerPaused()),
+                  ),
+                  FloatingActionButton(
+                    child: const Icon(Icons.replay),
+                    onPressed: () =>
+                        context.read<TimerBloc>().add(const TimerReset()),
+                  ),
+                ],
+              TimerRunPause() => [
+                  FloatingActionButton(
+                    child: const Icon(Icons.play_arrow),
+                    onPressed: () =>
+                        context.read<TimerBloc>().add(const TimerResumed()),
+                  ),
+                  FloatingActionButton(
+                    child: const Icon(Icons.replay),
+                    onPressed: () =>
+                        context.read<TimerBloc>().add(const TimerReset()),
+                  ),
+                ],
+              TimerRunComplete() => [
+                  FloatingActionButton(
+                    child: const Icon(Icons.replay),
+                    onPressed: () =>
+                        context.read<TimerBloc>().add(const TimerReset()),
+                  ),
+                ]
+            }
+          ],
+        );
+      },
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.blue.shade50,
+            Colors.blue.shade500,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+String getEmojiByIndex(int index) => emojiCharacters[index];
+List<String> emojiCharacters = [
+  '😀',
+  '😃',
+  '😄',
+  '😁',
+  '😆',
+  '😅',
+  '😂',
+  '🤣',
+  '😊',
+  '😇',
+  '🙂',
+  '🙃',
+  '😉',
+  '😌',
+  '😍',
+  '😘',
+  '😗',
+  '😙',
+  '😚',
+  '😋',
+  '😛',
+  '😝',
+  '😜',
+  '🤪',
+  '😎',
+  '🤓',
+  '😒',
+  '😞',
+  '😔',
+  '😟',
+  '😕',
+  '🙁',
+  '😣',
+  '😖',
+  '😫',
+  '😩',
+  '😤',
+  '😠',
+  '😡',
+  '😶',
+  '😐',
+  '😑',
+  '😯',
+  '😦',
+  '😧',
+  '😮',
+  '😲',
+  '😵',
+  '😳',
+  '😱',
+  '😨',
+  '😰',
+  '😢',
+  '😥',
+  '🤤',
+  '😭',
+  '😓',
+  '😪',
+  '😴',
+  '🙄',
+  '🤔',
+  '🤥',
+  '😬',
+  '🤐',
+];
